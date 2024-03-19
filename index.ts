7a12c693efe98770417d53d94d8e0cdd4e5366c5
Thomas Basham <Bashamtg@gmail.com>
2024-03-18 17:22:49 -0700

    added boilerplat code for server

---
+++ index.ts
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// @ts-ignore
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// ERROR HANDLING
+
+// 404 error handler
+// @ts-ignore
+app.use((req, res, next) => {
+  const error = new Error(
+    "404 NOT FOUND: Are you sure you are supposed to be here???"
+  );
+  res.status = 404;
+  next(error);
+});
+
+// general error handler
+// @ts-ignore
+app.use((error, req, res, next) => {
+  res.status = error.status || 500;
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
